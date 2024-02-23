11027bc29bdc72f363a9e27d4d922bf12656ead5
kmzee-sm <133731546+kmzee-sm@users.noreply.github.com>
2024-02-24 05:19:23 +0530

    chngs

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import noesbackground from "../public/noteshero.webp";
+import searchIcon from "../public/search-icon.png";
 
 const HeroSection = () => {
   return (
@@ -9,6 +10,14 @@ const HeroSection = () => {
         <p>
           Save, Secure And <span>Easy.</span>
         </p>
+        <div className="searchContainer">
+          <div className="search-icon">
+            <img src={searchIcon} alt="search" />
+          </div>
+          <div className="searchInput">
+            <input type="text" placeholder="find your note..." />
+          </div>
+        </div>
       </div>
     </div>
   );
